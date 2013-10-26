012850d14cb11f3aeeaf6b3d9e691b3058bccb1a
Michael Baumberger <michael@moroku.com>
2013-10-26 12:18:50 +1100

    Add files for new sample app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewSampleApp::Application.config.secret_token = '5135c51e9d382d1dd648481475eb1fbdd25dd0d75887598c1960c8402c581c94205e997c057c9594711c90dfe64e58361aeacc94c407c0e50428583d84cc9855'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,59 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  let (:base_title) {'Ruby on Rails Tutorial Sample App'}
+
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit root_path
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit root_path
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do
+      visit help_path
+      expect(page).to have_title("#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit about_path
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the right title" do
+      visit about_path
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit contact_path
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+      visit contact_path
+      expect(page).to have_title("#{base_title} | Contact Us")
+    end
+  end
+end
