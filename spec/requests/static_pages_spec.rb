766f8909c6201ce42525dc35e974bd1d5447b582
Michael Baumberger <michael@moroku.com>
2013-10-27 20:19:16 +1100

    Make everything work with Rails 3.2 and links in the header and footer work

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,58 +2,64 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let (:base_title) {'Ruby on Rails Tutorial Sample App'}
-
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+
+    it "should have the h1 'Sample App'" do
       visit root_path
-      expect(page).to have_content('Sample App')
+      page.should have_selector('h1', text: 'Sample App')
     end
 
     it "should have the base title" do
       visit root_path
-      expect(page).to have_title("#{base_title}")
+      page.should have_selector('title',
+                                text: "Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
       visit root_path
-      expect(page).not_to have_title('| Home')
+      page.should_not have_selector('title', text: '| Home')
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+
+    it "should have the h1 'Help'" do
       visit help_path
-      expect(page).to have_content('Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
-    it "should have the right title" do
+    it "should have the title 'Help'" do
       visit help_path
-      expect(page).to have_title("#{base_title} | Help")
+      page.should have_selector('title',
+                                text: "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
+
+    it "should have the h1 'About'" do
       visit about_path
-      expect(page).to have_content('About Us')
+      page.should have_selector('h1', text: 'About Us')
     end
 
-    it "should have the right title" do
+    it "should have the title 'About Us'" do
       visit about_path
-      expect(page).to have_title("#{base_title} | About")
+      page.should have_selector('title',
+                                text: "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
   describe "Contact page" do
-    it "should have the content 'Contact Us'" do
+
+    it "should have the h1 'Contact'" do
       visit contact_path
-      expect(page).to have_content('Contact Us')
+      page.should have_selector('h1', text: 'Contact')
     end
 
-    it "should have the right title" do
+    it "should have the title 'Contact'" do
       visit contact_path
-      expect(page).to have_title("#{base_title} | Contact Us")
+      page.should have_selector('title',
+                                text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
